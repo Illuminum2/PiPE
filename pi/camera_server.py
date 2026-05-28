f72bfeee0dd39a9c908b902cb42f10bc3971246f
Illuminum2 <trost.zureden08@icloud.com>
2026-05-28 21:26:24 +0200

    refactor: improve camera_server

--- pi/camera_server.py
+++ pi/camera_server.py
@@ -5,27 +5,21 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--host", default="10.55.0.1", help="pi ethernet host")
-parser.add_argument("--port", default="5555", help="pi ethernet port")
+parser.add_argument("--host", default="10.55.0.1", help="pi host")
+parser.add_argument("--port", default="5555", help="pi port")
 parser.add_argument("--width", type=int, default=640, help="camera width")
 parser.add_argument("--height", type=int, default=360, help="camera height")
 parser.add_argument("--fps", type=int, default=30, help="camera fps")
-parser.add_argument("--quality", type=int, default=45, help="camera transmission quality")
+parser.add_argument("--quality", type=int, default=45, help="camera jpg quality")
 args = parser.parse_args()
-
-HOST = args.host
-PORT = args.port
-SIZE = (args.width, args.height)
-FPS = args.fps
-QUALITY = args.quality
 
 
 def main():
     cam = Picamera2()
     cam.configure(
         cam.create_video_configuration(
-            main={"size": SIZE, "format": "RGB888"},
-            controls={"FrameRate": FPS},
+            main={"size": (args.width, args.height), "format": "RGB888"},
+            controls={"FrameRate": args.fps},
             buffer_count=1,
         )
     )
@@ -34,9 +28,9 @@ def main():
     ctx = zmq.Context()
     sock = ctx.socket(zmq.PUB) # zmq publish mode
     sock.setsockopt(zmq.SNDHWM, 1) # limit the send queue to only the latest element
-    sock.bind(f"tcp://{HOST}:{PORT}")
+    sock.bind(f"tcp://{args.host}:{args.port}")
 
-    print(f"Camera server running on {HOST}:{PORT}")m
+    print(f"Camera server running on {args.host}:{args.port}")
 
     try:
         while True:
@@ -44,10 +38,11 @@ def main():
 
             # https://stackoverflow.com/questions/59259786/how-to-encode-opencv-image-as-bytes-using-python
             # https://stackoverflow.com/questions/64342838/why-is-cv2-imencodejpg-seemingly-changing-the-color-of-an-image
-            ok, jpg = cv2.imencode(".jpg", rgb, [cv2.IMWRITE_JPEG_QUALITY, QUALITY])
+            ok, jpg = cv2.imencode(".jpg", rgb, [cv2.IMWRITE_JPEG_QUALITY, args.quality])
 
             if ok:
                 sock.send(jpg.tobytes())
+
     finally:
         sock.close(0)
         ctx.term()
