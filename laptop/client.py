d054fc23ef960a8616956ae7a642071db37a4d22
Illuminum2 <trost.zureden08@icloud.com>
2026-06-13 23:52:47 +0200

    feat: add flip option

--- laptop/client.py
+++ laptop/client.py
@@ -7,7 +7,7 @@ PI_HOST = "10.12.194.1"
 PI_PORT = 5555
 
 
-def frames_from_pi(host=PI_HOST, port=PI_PORT):
+def frames_from_pi(host=PI_HOST, port=PI_PORT, flip=True):
     ctx = zmq.Context()
     sock = ctx.socket(zmq.SUB)  # zmq subscriber mode
     sock.setsockopt(zmq.CONFLATE, 1)  # limit receive queue to only the latest element
@@ -20,6 +20,9 @@ def frames_from_pi(host=PI_HOST, port=PI_PORT):
 
             # https://stackoverflow.com/questions/49511753/python-byte-image-to-numpy-array-using-opencv
             frame = cv2.imdecode(np.frombuffer(jpg, np.uint8), cv2.IMREAD_COLOR)
+
+            if flip:
+                frame = cv2.flip(frame, 1)
 
             if frame.any():
                 yield frame
